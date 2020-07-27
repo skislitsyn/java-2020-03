02a62601fb8c6ae9214c7a807d99c2d2e90b4882
Sergey Kislitsyn <kislitsyn.s.a@gmail.com>
2020-07-27 22:18:07 +0300

    ДЗ №14

---
+++ hw14-warMvc/src/main/java/ru/otus/WebConfig.java
@@ -0,0 +1,72 @@
+package ru.otus;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import ru.otus.core.model.User;
+import ru.otus.hibernate.HibernateUtils;
+import skislitsyn.core.model.Address;
+import skislitsyn.core.model.Phone;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    public WebConfig(ApplicationContext applicationContext) {
+	this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+	SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	templateResolver.setApplicationContext(this.applicationContext);
+	templateResolver.setPrefix("/WEB-INF/templates/");
+	templateResolver.setSuffix(".html");
+	templateResolver.setTemplateMode(TemplateMode.HTML);
+	templateResolver.setCacheable(true);
+	templateResolver.setCharacterEncoding("UTF-8");
+	return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	templateEngine.setTemplateResolver(templateResolver());
+	return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+	ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	viewResolver.setTemplateEngine(templateEngine());
+	viewResolver.setOrder(1);
+	viewResolver.setCharacterEncoding("UTF-8");
+	return viewResolver;
+    }
+
+    @Bean
+    public SessionFactory sessionFactoryResolver() {
+	SessionFactory sessionFactory = HibernateUtils.buildSessionFactory("hibernate.cfg.xml", User.class,
+		Address.class, Phone.class);
+	return sessionFactory;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/");
+    }
+
+}
---
+++ hw14-warMvc/src/main/java/skislitsyn/core/model/Phone.java
@@ -0,0 +1,52 @@
+package skislitsyn.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phones")
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "number")
+    private String number;
+
+    public Phone() {
+    }
+
+    public Phone(long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
